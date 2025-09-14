6eaddc4268b19236ad6809d34409138c64eb1093
Alisha-safdar <alishasafdar407@gmail.com>
2025-09-14 09:57:50 +0500

    node.js

--- node.js
+++ node.js
@@ -1,24 +1,4 @@
-// Contact form popup
 document.getElementById("contact-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("Thank you, Alisha will get back to you soon!");
+  alert("Thank you for reaching out!");
 });
-
-// Scroll reveal animation
-const reveals = document.querySelectorAll(".reveal");
-
-function revealSections() {
-  for (let i = 0; i < reveals.length; i++) {
-    const windowHeight = window.innerHeight;
-    const elementTop = reveals[i].getBoundingClientRect().top;
-    const revealPoint = 150;
-
-    if (elementTop < windowHeight - revealPoint) {
-      reveals[i].classList.add("show");
-    } else {
-      reveals[i].classList.remove("show");
-    }
-  }
-}
-
-window.addEventListener("scroll", revealSections);
